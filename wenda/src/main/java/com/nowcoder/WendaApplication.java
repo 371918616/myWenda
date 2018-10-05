74aef08c5f49a2aba8e1e8a866d95261f2b8f22c
ed cty <371918616@qq.com>
2018-10-05 13:16:20 +0800

    完成首页后台

--- wenda/src/main/java/com/nowcoder/WendaApplication.java
+++ wenda/src/main/java/com/nowcoder/WendaApplication.java
@@ -1,10 +1,19 @@
 package com.nowcoder;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
+
 public class WendaApplication {
+
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WendaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WendaApplication.class, args);
--- wenda/src/test/java/com/nowcoder/InitDatabaseTests.java
+++ wenda/src/test/java/com/nowcoder/InitDatabaseTests.java
@@ -1,6 +1,8 @@
 package com.nowcoder;
 
+import com.nowcoder.dao.QuestionDAO;
 import com.nowcoder.dao.UserDAO;
+import com.nowcoder.model.Question;
 import com.nowcoder.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,15 +11,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Sql("/init-schema.sql")
 
 public class InitDatabaseTests {
 
 	@Autowired
 	UserDAO userDAO;
+
+	@Autowired
+	QuestionDAO questionDAO;
 
 	@Test
 	public void initDatabase() {
@@ -25,11 +32,24 @@ public class InitDatabaseTests {
 		for(int i = 0 ; i < 11 ; ++ i){
 			User user = new User();
 			user.setHeadUrl(String.format("http://images.nowcoder.com/head/%dt.png" , random.nextInt(1000)));
-			user.setName(String.format("USER%d" , i));
+			user.setName(String.format("USER%d" , i+20));
 			user.setPassword("");
 			user.setSalt("");
 			userDAO.addUser(user);
+
+			Question question = new Question();
+			question.setCommentCount(i);
+			Date date = new Date();
+			date.setTime(date.getTime() + 1000 * 3600 * i);
+			question.setUserId(i+1);
+			question.setCreatedDate(date);
+			question.setTitle(String.format("TITLE%d" , i));
+			question.setContent(String.format("aaaaaa+content%d" , i));
+
+			questionDAO.addQuestion(question);
 		}
+
+		System.out.println(questionDAO.selectLatestQuestions(1 , 0 , 5));
 	}
 
 }
